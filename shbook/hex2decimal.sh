aafd1dbe055bdf7a701bf717ec832c3e36721db3
Richard Milne <richmilne@hotmail.com>
2019-10-05 11:21:16 +0200

    Updated header comments - which mainly involved correcting the book's name.
    Even though the files were delivered with a book entitled "Portable Shell
    Programming", the files referred to a book entitled "Shell Programming
    Examples" (the working title before publication?)

--- shbook/hex2decimal.sh
+++ shbook/hex2decimal.sh
@@ -2,8 +2,8 @@
 #
 # Copyright 1995, by Hewlett-Packard Company
 #
-# The code in this file is from the book "Shell Programming
-# Examples" by Bruce Blinn, published by Prentice Hall.
+# The code in this file is from the book "Portable Shell
+# Programming" by Bruce Blinn, published by Prentice Hall.
 # This file may be copied free of charge for personal,
 # non-commercial use provided that this notice appears in
 # all copies of the file.  There is no warranty, either
